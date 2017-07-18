87ccb011c3ca72998cbcdacb3d69b3b5e24b357b
Lorenzo Di Tucci <lorenzo.ditucci@mail.polimi.it>
2017-07-18 15:15:22 -0700

    here keep only the solved BUG with ap_int

--- makefile_vector_add/from_forum/setup_xocc.sh
+++
@@ -1,9 +0,0 @@
-#BASH shell setup script for compiling application using xocc and running
-#CPU and hardware emulation directly from command line
-#
-#set XILINX_OPENCL to SDACCel installation directory
-export XILINX_OPENCL=/opt/Xilinx/SDAccel/2015.4
-
-source $XILINX_OPENCL/settings64.sh
-export LD_LIBRARY_PATH=$XILINX_OPENCL/runtime/lib/x86_64:$LD_LIBRARY_PATH
-export PATH=$XILINX_OPENCL/lnx64/tools/gcc/bin/:$PATH
